b5ba878367774881e1ed0f9dc95bdcb5c1210719
Craig Fontenot <craigjosephphd@gmail.com>
2024-07-30 20:04:08 -0500

    Initial Commit

---
+++ SoundVizFFT/SoundVizFFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundVizFFTApp.swift
+//  SoundVizFFT
+//
+//  Created by cj on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundVizFFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
